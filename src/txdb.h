9151b23b39ffae4decdcd9a44ebb950d256d5777
growcoindev <growcoindev@gmail.com>
2017-11-04 11:59:51 -0400

    grow-qt
    grow-qt v.1.0.0.0

---
+++ src/txdb.h
@@ -0,0 +1,11 @@
+// 
+
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_TXDB_H
+#define BITCOIN_TXDB_H
+
+#include "txdb-leveldb.h"
+
+#endif  // BITCOIN_TXDB_H
